25232ab71aa50819c4141e0f096cf7a0dbf5cbf5
reshampitale <85833711+reshampitale@users.noreply.github.com>
2021-07-21 11:33:39 +0530

    hi 1st commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using GitLocalChangePullDemo;
+
+using System;
+
+namespace GirCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("walterlv's automatic git command");
+
+            var git = new CommandRunner("git", @"C:\Users\ReshamP\Desktop\GitLocalChangePullDemo");
+            var status = git.Run("add .");
+
+            Console.WriteLine(status);
+            Console.WriteLine("Exit the program by Enter... ");
+
+
+            Console.ReadLine();
+        }
+    }
+}
