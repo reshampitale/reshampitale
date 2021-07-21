3315b591cfe020bd7b4910c00ed2e5627fa7f962
reshampitale <85833711+reshampitale@users.noreply.github.com>
2021-07-21 12:59:22 +0530

    My 3rd commit message

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace GirCore
             Console.WriteLine("Hello World!");
             string gitCommand = @"C:\Program Files\Git\cmd\git.exe";
             string gitAddArgument = "add -A";
-            string commitMessage = "My 2nd commit message";
+            string commitMessage = "My 3rd commit message";
             string gitCommitArgument = String.Format("commit -a -m \"{0}\"", commitMessage);
             string gitPushArgument = "push";
 
@@ -21,7 +21,7 @@ namespace GirCore
             Process.Start(gitCommand, gitAddArgument); // Runs: git add -A
             Process.Start(gitCommand, gitCommitArgument); // Runs: git commit -a -m "Your commit message"
             Process.Start(gitCommand, gitPushArgument); // Runs: git push
-            ///jkhjkhj
+           
             // var git = new CommandRunner(@"C:\Program Files\Git\cmd\git.exe", @"C:\Users\ReshamP\Desktop\GitLocalChangePullDemo");
             //git.Run("git remote add origin https://github.com/reshampitale/reshampitale.git");
             // git.Run("git commit -m "+ commitMessage + "");
