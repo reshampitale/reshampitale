67c16dfcc1312a801d38d4f9536136764581783f
reshampitale <85833711+reshampitale@users.noreply.github.com>
2021-07-21 12:56:17 +0530

    My 2nd commit message

--- Program.cs
+++ Program.cs
@@ -12,17 +12,22 @@ namespace GirCore
             Console.WriteLine("Hello World!");
             string gitCommand = @"C:\Program Files\Git\cmd\git.exe";
             string gitAddArgument = "add -A";
-            string commitMessage = "My 1sttt commit message";
+            string commitMessage = "My 2nd commit message";
             string gitCommitArgument = String.Format("commit -a -m \"{0}\"", commitMessage);
             string gitPushArgument = "push";
+
+
 
             Process.Start(gitCommand, gitAddArgument); // Runs: git add -A
             Process.Start(gitCommand, gitCommitArgument); // Runs: git commit -a -m "Your commit message"
             Process.Start(gitCommand, gitPushArgument); // Runs: git push
-            //var git = new CommandRunner("https://github.com/reshampitale/reshampitale.git", @"https://github.com/reshampitale/reshampitale.git");
-            //var status = git.Run("remote add");
-
-            //Console.WriteLine(status);
+            ///jkhjkhj
+            // var git = new CommandRunner(@"C:\Program Files\Git\cmd\git.exe", @"C:\Users\ReshamP\Desktop\GitLocalChangePullDemo");
+            //git.Run("git remote add origin https://github.com/reshampitale/reshampitale.git");
+            // git.Run("git commit -m "+ commitMessage + "");
+            // git.Run("git branch -M main");
+            // git.Run("git push - u origin main");
+            ////Console.WriteLine(status);
             Console.WriteLine("Exit the program by Enter... ");
 
 
